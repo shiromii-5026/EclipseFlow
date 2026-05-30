f8eb82410c0e292e08acc6f9048c813d216a03ae
shiromii-5026 <1217766211@qq.com>
2026-05-31 00:58:39 +0800

    fix: add auth package to MapperScan

--- eclipse-flow-backend/src/main/java/net/togogo/eclipseflowbackend/EclipseFlowBackendApplication.java
+++ eclipse-flow-backend/src/main/java/net/togogo/eclipseflowbackend/EclipseFlowBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("net.togogo.eclipseflowbackend.mapper")
+@MapperScan({"net.togogo.eclipseflowbackend.mapper", "net.togogo.eclipseflowbackend.auth"})
 public class EclipseFlowBackendApplication {
 
     public static void main(String[] args) {
